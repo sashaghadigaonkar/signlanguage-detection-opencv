22561e034ea011d8ec1eb60524881f987cdc88b2
sashaghadigaonkar <sashagladdy11@gmail.com>
2025-02-20 14:22:04 +0530

    second commit

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier("Model/keras_model.h5","Model/labels.txt")
+classifier = Classifier("Model/fixed_keras_model.h5","Model/labels.txt")
 
 offset = 20
 imgSize = 300
